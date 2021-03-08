03f43cdc4a676b436981608668357299468fd42d
Carly Peyton <carlypeyton@gmail.com>
2021-03-08 13:45:39 -0500

    Add more burgers to seeds file.

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,4 +6,10 @@ INSERT INTO burgers (burger_name, devoured)
 VALUE ("Beyond Burger", FALSE);
 
 INSERT INTO burgers (burger_name, devoured)
+VALUE ("Avocado Burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUE ("Impossible Burger", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
 VALUE ("Mushroom Burger", TRUE);
