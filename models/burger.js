68b696acf2db6a9705d864f2cd6f3f0c116b17cc
Carly Peyton <carlypeyton@gmail.com>
2021-03-08 11:11:41 -0500

    Update burger model.

--- models/burger.js
+++ models/burger.js
@@ -1,17 +1,16 @@
 const orm = require("../config/orm.js");
 
 const burger = {
-  all: (cb) => {
+  all(cb) {
     orm.all('burgers', (res) => cb(res));
   },
-  // The variables cols and vals are arrays.
-  create: (cols, vals, cb) => {
+  create(cols, vals, cb) {
     orm.create('burgers', cols, vals, (res) => cb(res));
   },
-  update: (objColVals, condition, cb) => {
+  update(objColVals, condition, cb) {
     orm.update('burgers', objColVals, condition, (res) => cb(res));
   },
-  delete: (condition, cb) => {
+  delete(condition, cb) {
     orm.delete('burgers', condition, (res) => cb(res));
   },
 };
