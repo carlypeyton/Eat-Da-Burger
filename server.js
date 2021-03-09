0b301aef35e2c786940b5834e1ca13b765edcddd
Carly Peyton <carlypeyton@gmail.com>
2021-03-08 20:22:01 -0500

    Update server comments.

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const express = require("express");
 const exphbs = require('express-handlebars');
 
 //PORT and Express
-const PORT = process.env.PORT || 3000;
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 //Set Routes and Data Parsing
-app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//Static directory
+app.use(express.static("public"));
 
 //Handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
